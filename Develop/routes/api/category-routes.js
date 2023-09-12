0d9035c1b3afd1e814042523754463a670a7f1da
PeterJanasz <petertjanasz@gmail.com>
2023-09-12 16:16:47 -0700

    created tage routes CRUD functions.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -24,7 +24,7 @@ router.get('/:id', async (req, res) => {
       include: [{ model: Product }]
     });
 
-    if (!categoryData) {
+    if (!categoryData[0]) {
       res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
@@ -56,7 +56,7 @@ router.put('/:id', async (req, res) => {
       },
     });
     if (!categoryData) {
-      res.status(404).json({ message: 'No category with this id!' });
+      res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
     res.status(200).json(categoryData);
