8d6f236ea6ad3b1e320656832f240d61f5835830
PeterJanasz <petertjanasz@gmail.com>
2023-09-12 15:06:11 -0700

    created delete function in product routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { INTEGER } = require('sequelize');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
@@ -23,7 +24,20 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-  
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -36,7 +50,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+     product_name: req.body.product_name,
+      price: req.body.pric,
+      stock: req.body.stock,
+      tagIds: req.body.tagIds,
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -103,8 +122,23 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+  const productData = await Product.destroy({
+    where: {
+      product_id: req.params.product_id,
+    },
+  });
+  if (!productData) {
+    res.status(404).json({ message: 'No product found with this id!' });
+      return;
+  }
+
+    res.status(200).json(productData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 module.exports = router;
